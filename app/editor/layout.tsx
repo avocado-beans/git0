fe8ca906b7035e9ae9ed6df5f5d2871f9da6b17f
Estifanos <tolemariamestifanos@gmail.com>
2024-10-25 17:15:23 +0300

    Fake commit

---
+++ app/editor/layout.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+    title: "Editor Page"
+}
+
+export default function Layout({ children }: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang="en">
+          <body>
+            {children}
+          </body>
+        </html>
+      );
+}
