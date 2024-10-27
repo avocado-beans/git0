78016e206ae8a7b11119fae06d87b8c370310d7d
Estifanos <tolemariamestifanos@gmail.com>
2024-10-27 15:38:07 +0300

    Fake commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import { useEffect, useState, useRef, Suspense } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 
 import { EditorView } from 'codemirror';
 import { langs } from '@uiw/codemirror-extensions-langs';
 
-import { Gemini, GeminiResponse } from './editor/gemini';
+import { Gemini, GeminiResponse } from './preview/gemini';
 
 const customLinterTheme = EditorView.theme({
   'span.ͼm': {
@@ -36,7 +36,8 @@ export default function App() {
     const fetchData = async () =>{
       setIsLoading(true);
       try {
-        const res: GeminiResponse = await Gemini(selectedCode.length > 0 ? selectedCode+ '\n' +prompt : prompt)
+        console.log(selectedCode, prompt)
+        const res: GeminiResponse = await Gemini(selectedCode.length > 0 ? `${selectedCode}\n${prompt}` : prompt)
         setGeminiCode(res?.candidates[0].content.parts[0].text)
       } catch (err) {
         console.log('Error fetching data:', err)
@@ -53,55 +54,69 @@ export default function App() {
   }, [trigger])
 
   return (
-    <div className='flex justify-evenly'>
-    <div className='flex flex-col w-[50%] p-[10px] gap-[10px]'>
-    <form onSubmit={(e) => {e.preventDefault(); setTrigger(true)}}>
-      <input className='w-[100%] text-wrap' type="text" placeholder="Enter instructions" onChange={(e) => {setPrompt(e.target.value)}}/>
-    </form>
+    <div className='flex flex-col justify-between'>
+      {/* Actual code stuff */}
+      <div className='flex p-[10px] gap-[10px] h-[85vh]'>        
+        <div className='flex flex-col w-[60%]'>
+            <CodeMirror
+                className='border-y border-x-2'
+                value={selectedCode}
+                maxHeight='40vh'
+                extensions={[
+                  langs.tsx(),
+                  customLinterTheme,
+                  EditorView.lineWrapping
+                ]}
 
-    <CodeMirror
-      className='border-2'
-      value={selectedCode}
-      extensions={[
-        langs.tsx(),
-        customLinterTheme,
-        EditorView.lineWrapping
-      ]}
+                onChange={(e) => {setSelectedCode(e)}}
+            />           
+            <CodeMirror
+                className='border-y border-x-2 overflow-auto'
+                value={code}
+                extensions={[
+                  langs.tsx(),
+                  customLinterTheme,
+                  EditorView.lineWrapping
+                ]}
+                
+                onChange={(e) => {setCode(e)}}
+                onMouseUp={handleSelection}
+                onKeyUp={handleSelection}
+                ref={codeRef}
+            />
+        </div>
 
-      onChange={(e) => {setSelectedCode(e)}}
-      />
-
-    <CodeMirror
-      className='border-2'
-      value={code}
-      extensions={[
-        langs.tsx(),
-        customLinterTheme,
-        EditorView.lineWrapping
-      ]}
-      
-      onChange={(e) => {setCode(e)}}
-      onMouseUp={handleSelection}
-      onKeyUp={handleSelection}
-      ref={codeRef}
-      />
-
-    </div>
-
-    <div className='flex flex-col w-[50%] p-[10px] gap-[10px] overflow-y-auto h-[100vh]'>
-      <span>[Gemini Code Review]:</span>
-      {isLoading ? 
-      <div>Thinking...</div> : 
-      <CodeMirror
-        className='border-2'
-        value={geminiCode}
-        extensions={[
-          langs.tsx(),
-          customLinterTheme,
-          EditorView.lineWrapping
-        ]}
-        />}
+        <div className='flex flex-col w-[40%]'>
+          {isLoading ? 
+          <div>Thinking...</div> : 
+          <div className='border-2 overflow-auto'>
+            <CodeMirror
+              value={geminiCode}
+              extensions={[
+                langs.markdown(),
+                customLinterTheme,
+                EditorView.lineWrapping
+              ]}
+              />
+          </div>
+          // <iframe height={'100%'} className='border-2' src="http://localhost:3000/preview"></iframe>
+          }
+        </div>
       </div>
-  </div>
+      
+      {/* Nav bar */}
+      <div className='flex bottom-0 p-[10px] gap-[10px] w-[100%] bg-white border-t-2'>
+        <div className='w-[60%]'>
+          <form className='flex items-start justify-start gap-[10px]' onSubmit={(e) => {e.preventDefault(); setTrigger(true)}} >
+            <button className='px-[40px] py-[10px] bg-purple-600 text-white border-2'>prompt</button>
+            <textarea className='flex-1 resize-none outline-none text-wrap w-[100%] min-h-[100%] px-[10px] border-2' placeholder="enter prompt" onChange={(e) => {setPrompt(e.target.value)}}/>
+          </form>
+        </div>
+
+        <div className='w-[40%] border-2'>
+        <span>[Gemini Code Review]:</span>
+        </div>
+      </div>
+    </div>
   )
 }
